aa9e8cfbc9242c23622ee0b347654f55a49d633a
Ciar Daire <92127545+CiarDaire@users.noreply.github.com>
2025-04-16 11:57:11 +1200

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,19 @@
+
+# duplicates
+my_set = {"red", "blue", "red"}
+
+print("\n", my_set, "\n")
+
+
+# numbers not working
+numbers = {1, 5, 3, 7, 400}
+
+print("\n", numbers, "\n")
+
+# strings
+
+strings = {"a", "b", "c", "d"}
+
+print("\n", strings, "\n")
+
+
